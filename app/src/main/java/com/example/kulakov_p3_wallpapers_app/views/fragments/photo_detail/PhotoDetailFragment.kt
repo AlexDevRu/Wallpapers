55a4199d05392f2c99de351556880abfa32fce36
Alexander <a.kulakov.vironit@vironit.ru>
2021-07-26 18:50:36 +0300

    fix: save animation state im motion layout

--- app/src/main/java/com/example/kulakov_p3_wallpapers_app/views/fragments/photo_detail/PhotoDetailFragment.kt
+++ app/src/main/java/com/example/kulakov_p3_wallpapers_app/views/fragments/photo_detail/PhotoDetailFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.navArgs
 import com.example.kulakov_p3_wallpapers_app.R
